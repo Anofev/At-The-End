3be264059bc5814cec008c836f686b5a0860da97
Tanaka_Dorei <tlranfdlsrks4245@gmail.com>
2022-01-02 22:57:31 +0900

    base

---
+++ Project/src/main/java/anofev/atthend/Atthend.java
@@ -0,0 +1,10 @@
+package anofev.atthend;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Atthend implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ Project/src/main/java/anofev/atthend/client/AtthendClient.java
@@ -0,0 +1,13 @@
+package anofev.atthend.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class AtthendClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
